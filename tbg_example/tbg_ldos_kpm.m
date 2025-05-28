4997e2e22996ad5fc2c5d5e971a9194786a62670
Jason Kaye <jkaye@flatironinstitute.org>
2025-05-28 18:32:53 -0400

    renaming files

--- tbg_example/tbg_ldos_kpm.m
+++ tbg_example/tbg_ldos_kpm.m
@@ -7,43 +7,28 @@
 % the value of the LDOS at a specific point for different values of p.
 
 % Input parameters
-pdata = 16000;  % Chebyshev degree computed in data file
+filename = 'r800_p16000_ldos.mat';
+dE = 0.005;      % Energy grid spacing
 
-rs = [100 200 400 800 1600 3200];
-ps = [500, 1000, 2000, 4000, 8000, 16000];  % Polynomial degrees
-E = -0.43; % Pick specific energy to measure
+ps = [500,1000,2000,4000];  % Polynomial degrees
 
 addpath('../kpm')
+load(['../cheb_wgts_data/',filename]); % Load parameters and Chebyshev weights from file
 
-ldos_val = zeros(length(ps),length(rs));
-for j=1:length(rs)
-    r = rs(j);
-    filename = ['r',num2str(r),'_p',num2str(pdata),'_ldos.mat'];
-    load(['../cheb_wgts_data/',filename]); % Load parameters and Chebyshev weights from file
+E = (-E_range):dE:E_range; % Energy grid
 
-    % Compute local densities of states
-    for i=1:length(ps)
-        p = ps(i);
-        ldos_val(i,j) = kpm_ldos(p, E/E_range, cheb_wgts(1:p));
-    end
-end
-
-% Compute self-convergence error
-err = abs(ldos_val(2:end,:) - ldos_val(1:end-1,:));
 figure(5);
-set(gcf,'position',[100,100,1000,800])
-loglog(ps(1:end-1),err,'.-','linewidth',1.5,'markersize',20); hold on
-loglog(ps(2:5),1e2*ps(2:5).^-2,'--k'); hold off
-xlabel('$p$','interpreter','latex')
-ylabel('Self-convergence error')
-set(gca,'fontsize',15)
-ylim([1e-8, 1e-1])
-xlim([5e2 1.5e4])
+% Compute local densities of states
+for i=1:length(ps)
+    p = ps(i);
+    ldos = kpm_ldos(p, E/E_range, cheb_wgts(1:p));
 
-% Legend with text 'r = {rs(1),...,rs(end)}'
-legend_str = cell(1,length(rs));
-for j=1:length(rs)
-    legend_str{j} = ['r = ',num2str(rs(j))];
+    plot(E, ldos, '.-'); hold on
+    xlim([-2 1])
 end
-legend(legend_str,'location','southwest')
+hold off
+
+legend(string(num2cell(ps)));
+xlabel('E')
+ylabel('LDOS(E)')
 set(gca,'fontsize',15)
--- tbg_example/tbg_ldos_kpm_pconverge.m
+++ tbg_example/tbg_ldos_kpm_pconverge.m
@@ -7,28 +7,43 @@
 % the value of the LDOS at a specific point for different values of p.
 
 % Input parameters
-filename = 'r800_p16000_ldos.mat';
-dE = 0.005;      % Energy grid spacing
+pdata = 16000;  % Chebyshev degree computed in data file
 
-ps = [500,1000,2000,4000];  % Polynomial degrees
+rs = [100 200 400 800 1600 3200];
+ps = [500, 1000, 2000, 4000, 8000, 16000];  % Polynomial degrees
+E = -0.43; % Pick specific energy to measure
 
 addpath('../kpm')
-load(['../cheb_wgts_data/',filename]); % Load parameters and Chebyshev weights from file
 
-E = (-E_range):dE:E_range; % Energy grid
+ldos_val = zeros(length(ps),length(rs));
+for j=1:length(rs)
+    r = rs(j);
+    filename = ['r',num2str(r),'_p',num2str(pdata),'_ldos.mat'];
+    load(['../cheb_wgts_data/',filename]); % Load parameters and Chebyshev weights from file
 
-figure(5);
-% Compute local densities of states
-for i=1:length(ps)
-    p = ps(i);
-    ldos = kpm_ldos(p, E/E_range, cheb_wgts(1:p));
-
-    plot(E, ldos, '.-'); hold on
-    xlim([-2 1])
+    % Compute local densities of states
+    for i=1:length(ps)
+        p = ps(i);
+        ldos_val(i,j) = kpm_ldos(p, E/E_range, cheb_wgts(1:p));
+    end
 end
-hold off
 
-legend(string(num2cell(ps)));
-xlabel('E')
-ylabel('LDOS(E)')
+% Compute self-convergence error
+err = abs(ldos_val(2:end,:) - ldos_val(1:end-1,:));
+figure(5);
+set(gcf,'position',[100,100,1000,800])
+loglog(ps(1:end-1),err,'.-','linewidth',1.5,'markersize',20); hold on
+loglog(ps(2:5),1e2*ps(2:5).^-2,'--k'); hold off
+xlabel('$p$','interpreter','latex')
+ylabel('Self-convergence error')
+set(gca,'fontsize',15)
+ylim([1e-8, 1e-1])
+xlim([5e2 1.5e4])
+
+% Legend with text 'r = {rs(1),...,rs(end)}'
+legend_str = cell(1,length(rs));
+for j=1:length(rs)
+    legend_str{j} = ['r = ',num2str(rs(j))];
+end
+legend(legend_str,'location','southwest')
 set(gca,'fontsize',15)
